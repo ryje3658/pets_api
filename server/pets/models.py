89f4373da1431c3ee0034b298384fad1307f9b1a
ryanj <jensenry@oregonstate.edu>
2021-05-06 18:02:38 -0400

    made a mistake in last commit, missed a comma

--- server/pets/models.py
+++ server/pets/models.py
@@ -15,7 +15,7 @@ class Pet(models.Model):
     availability = models.CharField(max_length=30)
     description = models.TextField()
     picture_primary = models.ImageField(blank=False)  # Primary picture is required
-    picture_second = models.ImageField(blank=, null=True)  # Second picture not required
+    picture_second = models.ImageField(blank=True, null=True)  # Second picture not required
     picture_third = models.ImageField(blank=True, null=True)  # Third picture not required
     gender = models.CharField(max_length=1)
     owner = models.ForeignKey('auth.User', related_name='pets', on_delete=models.CASCADE)
